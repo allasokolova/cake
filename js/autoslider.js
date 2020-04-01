fcd08eb30619ff7200d0018549506551bf382852
allasokolova <62805710+allasokolova@users.noreply.github.com>
2020-04-01 19:47:08 +0300

    Add files via upload

--- js/autoslider.js
+++ js/autoslider.js
@@ -7,7 +7,7 @@ function autoSlider (){
 	timer = setTimeout(function (){
 		var gallery = document.getElementById('gallery');
 		left = left - bias;
-		if (left < -postsCount*bias+10*bias) {
+		if (left < -postsCount*bias+30*bias) {
 			left = 0;
 			clearTimeout(timer);
 		}
